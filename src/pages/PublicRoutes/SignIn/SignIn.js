a4615d9ee203e1c654ea76f2a17f80a753b39690
nataliia-v <nataly.verbenskaya@gmail.com>
2019-12-10 00:47:48 +0200

    ASD-4: * user avatar as react component

--- src/pages/PublicRoutes/SignIn/SignIn.js
+++ src/pages/PublicRoutes/SignIn/SignIn.js
@@ -7,6 +7,8 @@ import { getIsLoadingUserLogin } from 'store/auth/selectors';
 
 import SignInForm from './components/SignInForm/SignInForm';
 
+import { ReactComponent as UserAvatar } from 'images/user.svg';
+
 import styles from './SignIn.module.scss';
 
 class SignIn extends Component {
@@ -21,11 +23,7 @@ class SignIn extends Component {
 
     return (
       <div className={styles.wrapper}>
-        <img
-          width="140px"
-          src="https://png2.cleanpng.com/sh/cd1d4c037673afffdd5fd49b8040345e/L0KzQYm3VsI3N6Z8i5H0aYP2gLBuTfF3aaVmip9Ac3X1PbT2jgB2fJZ3Rdtsb372PcT2hwR4aaNqRdZudnXvf8Hskr02amQ3T9VsOXPmQYbtV745P2M8SqkDMEG4Q4G3U8U1OGI9S6g3cH7q/kisspng-avatar-user-computer-icons-software-developer-5b327cc9cc15f7.872727801530035401836.png"
-          alt="user"
-        />
+        <UserAvatar className={styles.userAvatar} />
         <SignInForm
           onSubmit={this.onSubmit}
           isLoadingUserLogin={isLoadingUserLogin}
