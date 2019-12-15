6b06e8baaf0919be0384ada35c4ebca86f7bbf46
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-15 20:03:16 +0000

    Deploy nataliia-v/js-band-final-task to github.com/nataliia-v/js-band-final-task.git:gh-pages

---
+++ precache-manifest.8504de172600cfea255a5628e62fe55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fc365a7c7b316f101d6ba7e22422df",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "b8289846ae013a63c19d",
+    "url": "/js-band-final-task/static/css/main.83eaa135.chunk.css"
+  },
+  {
+    "revision": "b7284f1ee6cd75e5cf12",
+    "url": "/js-band-final-task/static/js/2.7fa422c6.chunk.js"
+  },
+  {
+    "revision": "b8289846ae013a63c19d",
+    "url": "/js-band-final-task/static/js/main.432a2e13.chunk.js"
+  },
+  {
+    "revision": "49af006cc0cf04963d6b",
+    "url": "/js-band-final-task/static/js/runtime-main.3be8268d.js"
+  },
+  {
+    "revision": "3abf384f059dd6129454fc4a56d8b52a",
+    "url": "/js-band-final-task/static/media/cart2.3abf384f.svg"
+  },
+  {
+    "revision": "8547ceae0d543a17c0ca6f55ff13e606",
+    "url": "/js-band-final-task/static/media/user.8547ceae.svg"
+  }
+]);
